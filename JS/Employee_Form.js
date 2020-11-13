bf48e32fdfd715fd67a667efbcba0f1b59a93975
lalit-003 <gahlawatl7@gmail.com>
2020-11-13 08:15:12 +0530

    Added functionalities to update and reset data

--- JS/Employee_Form.js
+++ JS/Employee_Form.js
@@ -28,9 +28,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const save = () => {
     try{
         let employeePayRollData = createEmployeePayRoll();
+        createAndUpdateStorage(employeePayRollData);
     }catch(e){
         return;
     }
+}
+
+//UC 4
+const createAndUpdateStorage = (employeePayrollData)=>{
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollList!=undefined){
+        employeePayrollList.push(employeePayrollData);
+    }else{
+        employeePayrollList=[employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
 }
 
 const createEmployeePayRoll = ()=>{
@@ -69,4 +82,31 @@ const getInputValueById = (id) => {
 const getInputElementValue = (id) => {
     let value = document.getElementById(id).value
     return value
+} 
+
+//UC5
+const resetForm = ()=>{
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2020');
+}
+const unsetSelectedValues = (propertyValue)=>{
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item=>{
+        item.checked=false;
+    })
+}
+const setTextValue=(id,value)=>{
+    const element = document.querySelector(id);
+    element.textContent=value;
+}
+const setValue = (id,value)=>{
+    const element = document.querySelector(id);
+    element.value=value;
 } 
