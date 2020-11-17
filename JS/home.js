cc9a66f73e5f71523feaadda4abae758a1ba17d8
lalit-003 <gahlawatl7@gmail.com>
2020-11-17 23:39:24 +0530

    Added functionality to employeePayrollHomePage  for adding employee payroll details in tabular format using JSON Object

--- JS/home.js
+++ JS/home.js
@@ -6,25 +6,68 @@ window.addEventListener("DOMContentLoaded", (event) => {
 const createInnerHtml = () => {
     const headerHtml =
         "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
-    const innerHtml = `
-      ${headerHtml}  
-      <tr>
-            <td>
-              <img class="profile"  src="../Assets/Ellipse -2.png" alt="" >
-            </td>
-            <td>Narayan Mahadevan</td>
-            <td>Male</td>
-            <td>
-              <div class="dept-label">HR</div>
-              <div class="dept-label">Finance</div>
-            </td>
-            <td>450000</td>
-            <td>17 Nov 2020</td>
-            <td>
-              <img name="1" onclick="remove(this)" src="../Assets/icons/delete-black-18dp.svg" alt="Delete">
-              <img name="1" onclick="update(this)" src="../Assets/icons/create-black-18dp.svg" alt="Edit">
-            </td>
-      </tr>
-      `;
-    document.querySelector("#table-display").innerHTML = innerHtml;
-}; 
+        let empPayrollList = createEmployeePayrollJSON();
+        let innerHtml = `${headerHtml}`;
+        for(const empPayrollData of empPayrollList){
+             innerHtml = `${innerHtml}
+             <tr>
+                     <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
+                     <td>${empPayrollData._name}</td>
+                     <td>${empPayrollData._gender}</td>
+                     <td>${getDeptHtml(empPayrollData._department)}</td>
+                     <td>${empPayrollData._salary}</td>
+                     <td>${empPayrollData._startDate}</td>
+                     <td>
+                         <img id="${empPayrollData._id}" onclick="remove(this)"  src="../Assets/icons/delete-black-18dp.svg" alt="delete">
+                         <img id="${empPayrollData._id}" onclick="update(this)"  src="../Assets/icons/create-black-18dp.svg" alt="edit">
+                     </td>
+                 </tr>
+             `;
+             document.querySelector('#table-display').innerHTML = innerHtml;
+        }
+        document.querySelector("#table-display").innerHTML = innerHtml;
+     }
+     const getDeptHtml = (deptList) => {
+         let deptHtml = '';
+         for(const dept of deptList){
+             deptHtml = `${deptHtml}<div class="dept-label">${dept}</div>`
+         }
+         return deptHtml;
+     }
+     const createEmployeePayrollJSON = () => {
+         let empPayrollListLocal = [
+             {
+                 _name:'Bittu Dalal',
+                 _gender:'male',
+                 _department:['Engineering',
+                                'Finance'],
+                 _salary:'500000',
+                 _startDate:'29 Oct 2019',
+                 _note: '',
+                 _id: new Date().getTime(),
+                 _profilePic:'../Assets/Ellipse -2.png'
+             },
+             {
+                 _name:'Ankita Saini',
+                 _gender:'female',
+                 _department:['Sales'],
+                 _salary:'400000',
+                 _startDate:'28 Feb 2019',
+                 _note:'',
+                 _id: new Date().getTime()+1,
+                 _profilePic:'../Assets/Ellipse -1.png'
+             },
+             {
+                 _name:'Shivam Bhargava',
+                 _gender:'male',
+                 _department:['Finance'],
+                 _startDate:'17 Nov 2018',
+                 _note:'',
+                 _salary:'600000',
+                 _id: new Date().getTime()+2,
+                 _profilePic:'../Assets/Ellipse -3.png'
+             }
+         ]
+         return empPayrollListLocal;
+     } 
+   
